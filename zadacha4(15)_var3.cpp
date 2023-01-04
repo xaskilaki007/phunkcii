2736a2145c88d8a47959d8461e36e88249360826
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-01-04 18:20:16 +0300

    Update zadacha4(15)_var3.cpp

--- zadacha4(15)_var3.cpp
+++ zadacha4(15)_var3.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-float Calc(float a, float b, int Op)
+float Leng(float xA, float yA, float xB, float yB)
 {
-    if (Op == 1) return a - b;
-    if (Op == 2) return a * b;
-    if (Op == 3) return a / b;
-    return a + b;
+    return sqrt((xA-xB)*(xA-xB) + (yA-yB)*(yA-yB));
 }
+
 int main()
 {
-    float a, b;
-    int Op;
+
+    float xA, xB, xC, xD, yA, yB, yC, yD;
     
-    cout << "Vvedite chisla a i b:\n";
-    cin >> a >> b;
-    cout << "Vvedite nomer operacii:\n1 -\n2 *\n3 /\nOstalnoe +:\n";
-    cin >> Op;
-    cout << Calc(a,b,Op) << endl;
+    
+    cout << "Vvedite koordinaty tochki A:\n";
+    cin >> xA >> yA;
+    cout << "Vvedite koordinaty tochki B:\n";
+    cin >> xB >> yB;
+    cout << "Vvedite koordinaty tochki C:\n";
+    cin >> xC >> yC;
+    cout << "Vvedite koordinaty tochki D:\n";
+    cin >> xD >> yD;
+    
+    cout << "AB = " << Leng(xA,yA,xB,yB) << endl;
+    cout << "AC = " << Leng(xA,yA,xC,yC) << endl;
+    cout << "AD = " << Leng(xA,yA,xD,yD) << endl;
+    cout << "CD = " << Leng(xC,yC,xD,yD) << endl;
     return 0;
 }
