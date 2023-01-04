d91d5423b005937fc620941902f6056805f18a75
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-01-04 16:26:31 +0300

    Update 1zadanie var 3.cpp

--- 1zadanie var 3.cpp
+++ 1zadanie var 3.cpp
@@ -1,32 +1,21 @@
-/*
-В предыдущих домашних заданиях найти блоки кода, которые возможно выделить
-в функции и определить их в функции. Например, запись в файл, вычисление
-формул.
-*/
-#include<iostream>
-#include<cmath>
-floatLike(intx)
+//zadanie_14
+#include <iostream>
+#include <cmath>
+// Пользователь вводит цены 1 кг конфет и 1 кг печенья. Найдите стоимость:
+// а) одной покупки из 300 г конфет и 400 г печенья; б) трех покупок, каждая из 2 кг печенья и 1 кг 800 г конфет. 
+// Организовать задание с помощью функций.
+using namespace std;
+float price(float prConf, float prCookies, float wConf, float wCookies, int Number)
 {
-floaty;
-
-if(x>1)
-y=7+(23*x);
-elseif(x<1)
-y=((x+2)*32)-7;
-else
-y=4;
-
-std::cout«"Y:"«y;
-
-returnx;
+    float t = Number*(prConf * wConf + prCookies * wCookies); 
+    return t;
 }
-intmain()
-{
-floatx;
 
-std::cout«"X:";
-std::cin»x;
-std::cout«Like(x)«std::endl;
-
-return 0;
+int main() {
+    float k,cs;
+    cin >> k >> cs;
+    //Здание (a)
+    cout << price(k,cs,0.3,0.4,1) << endl;
+    // Задание (b)
+    std::cout << price(k,cs,1.8,2,3) << endl;
 }
